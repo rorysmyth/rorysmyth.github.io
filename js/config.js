53bde42fc9d9d9cf7186c9ea6e51b2b5af99facc
Rory Smyth <rsmyth@zendesk.com>
2014-11-17 16:59:21 +0000

    added list menu for IVR

--- js/config.js
+++ js/config.js
@@ -7,6 +7,10 @@ app.config(function($routeProvider,$resourceProvider) {
 		templateUrl: '/js/ivr/views/ivr-dropdown.html',
 		controller:'ivrMenuController'
 	})
+	.when('/ivr/list',{
+		templateUrl: '/js/ivr/views/ivr-list.html',
+		controller:'ivrMenuController'
+	})
 	.when('/settings',{
 		templateUrl: '/js/settings/views/settings.html',
 		controller: 'settingsController'
@@ -19,9 +23,9 @@ app.config(function($routeProvider,$resourceProvider) {
 		templateUrl: '/js/greetings/views/greetings.html',
 		controller: 'greetingsController'
 	})
-	// .when('/',{
-	// 	templateUrl: 'views/settings.html',
-	// 	controller: 'settingsController'
-	// })
+	.when('/',{
+		templateUrl: '/js/settings/views/settings.html',
+		controller: 'settingsController'
+	})
 
 })
